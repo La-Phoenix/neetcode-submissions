3ba81f6aea6ee2d90cfd6c4c2ed99718251e8e40
SAMUEL OKUNDALAIYE <106788926+La-Phoenix@users.noreply.github.com>
2026-08-19 12:20:48 +0100

    Add: remove-node-from-end-of-linked-list - submission-5

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-5.cs
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Build n distance
+        while (n != 0){
+            fast = fast.next; 
+            n--;
+        }
+
+        // Head has been removed
+        if (fast is null){
+            return head.next;
+        }
+
+        while (fast is not null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // Slow becomes the nth value (from the end)
+        // Remove from list
+        prev.next = slow.next;
+
+        return head;
+    }
+}
