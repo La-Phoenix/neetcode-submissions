4099a8fc78e8d70478880945e4ea81a671fd04a0
SAMUEL OKUNDALAIYE <106788926+La-Phoenix@users.noreply.github.com>
2026-08-23 20:37:45 +0100

    Add: add-two-numbers - submission-4

---
+++ Data Structures & Algorithms/add-two-numbers/submission-4.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode prev = null;
+        ListNode head = null;
+        int carry = 0;
+        while (l1 != null || l2 != null){
+            int sum = (l1 == null ? 0 
+            : l1.val) + (l2 == null ? 0 
+            : l2.val) + carry;
+
+            int newVal = sum % 10;
+            carry = sum / 10;
+            ListNode curr = new ListNode(newVal);
+            if (prev != null){
+                prev.next = curr;
+            } else {
+                head = curr;
+            }
+            prev = curr;
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+        }
+
+        if (carry > 0){
+            prev.next = new ListNode(carry);
+        }
+        return head;
+    }
+}
