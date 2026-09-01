de7e47bc28791bf2699e19085cdda59001525266
SAMUEL OKUNDALAIYE <106788926+La-Phoenix@users.noreply.github.com>
2026-09-01 10:17:29 +0100

    Add: same-binary-tree - submission-2

---
+++ Data Structures & Algorithms/same-binary-tree/submission-2.cs
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        if (p is null || q is null) return p == q;
+
+        bool isEqual = true;
+        if (p.val == q.val) {
+            isEqual = IsSameTree(p.left, q.left);
+            if (isEqual){
+                isEqual = IsSameTree(p.right, q.right);
+            }
+        } else {
+            isEqual = false;
+        }
+        return isEqual;
+    }
+}
